e5f154933345f2b6750b4b87cfe96b5013584d6b
Константин <tuder.muder@yandex.ru>
2025-12-22 21:56:26 +0400

    feat: сервис планировщик, который удаляет пользователей не подтвердивших свой email в течении 24 часов из keycloak

---
+++ user-service/src/main/java/org/pethealth/scheduled/ScheduledService.java
@@ -0,0 +1,56 @@
+package org.pethealth.scheduled;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.pethealth.utils.RealmUtil;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScheduledService {
+
+    private final RealmUtil realmUtil;
+
+    @Scheduled(cron = "0 */10 * * * *")
+    @Async
+    public void scheduled() {
+
+        int first = 0;
+        int max = 100;
+
+        log.debug("Start scheduled deleted users thread");
+
+        while (true) {
+
+            List<UserRepresentation> userRepresentations = realmUtil.getRealm()
+                    .users()
+                    .search(null, null, null, null, false, first, max, null, null);
+
+            if (userRepresentations.isEmpty()) {
+                break;
+            }
+
+            userRepresentations.forEach(user -> {
+                if (Boolean.FALSE.equals(user.isEmailVerified()) &&
+                        System.currentTimeMillis() - user.getCreatedTimestamp() > Duration.ofHours(24).toMillis()) {
+
+                    realmUtil.getRealm()
+                            .users()
+                            .delete(user.getId());
+                }
+                log.trace("Stop deleting users");
+
+            });
+            first+=max;
+
+        }
+        log.debug("Stop deleting users");
+    }
+}
