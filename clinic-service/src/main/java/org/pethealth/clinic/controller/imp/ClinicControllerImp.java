2d5323b425205b8eddc0e657e7cdd553a7f105d9
Константин <tuder.muder@yandex.ru>
2026-02-10 15:39:56 +0400

    style: unused import

--- clinic-service/src/main/java/org/pethealth/clinic/controller/imp/ClinicControllerImp.java
+++ clinic-service/src/main/java/org/pethealth/clinic/controller/imp/ClinicControllerImp.java
@@ -1,10 +1,5 @@
 package org.pethealth.clinic.controller.imp;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.pethealth.clinic.controller.ClinicController;
